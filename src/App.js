affbaedcb505911ef78a7e7a88fad6b3c6d009e7
Nagoor Patan <v-npatan@microsoft.com>
2022-09-15 16:34:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import TextField from "@mui/material/TextField";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container-fluid my-3">
+        {/* header container */}
+        <div className="container-lg text-center">
+          <h1 className="display-5 text-primary">
+            {" "}
+            <span>
+              <i class="fa-solid fa-list"></i>
+            </span>
+            Toll Managemen System
+          </h1>
+        </div>
+        <div className="container-fluid">
+          <div class="row align-items-start">
+            <div class="col align-self-start"><p className="lead text-muted">Toll Entries/Veichle Entries : </p></div>
+            <div class="col align-self-start"><input type="text" className="seatch" placeholder="Serach.." name="search" /></div>
+            <div class="col">One of three columns</div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
